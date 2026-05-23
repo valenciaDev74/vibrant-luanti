b171043dbb627af584d106ad4f936c79b014bca4
valenciaDev74 <jesus.valencia74@yahoo.com>
2026-05-23 18:29:31 -0400

    funcionando movimiento y sombras

--- nodes_shader/opengl_fragment.glsl
+++ nodes_shader/opengl_fragment.glsl
@@ -1,4 +1,7 @@
 VARYING_ vec4 varColor;
+VARYING_ vec3 vNormal;
+VARYING_ vec3 worldPosition;
+VARYING_ float nightRatio;
 
 #if USE_ARRAY_TEXTURE
 uniform sampler2DArray baseTexture;
@@ -6,6 +9,55 @@ VARYING_ vec3 varTexCoord;
 #else
 uniform sampler2D baseTexture;
 VARYING_ vec2 varTexCoord;
+#endif
+
+#ifdef ENABLE_DYNAMIC_SHADOWS
+uniform sampler2D ShadowMapSampler;
+uniform vec3 v_LightDirection;
+uniform float f_textureresolution;
+uniform mat4 m_ShadowViewProj;
+uniform float f_shadowfar;
+uniform float f_shadow_strength;
+uniform vec4 CameraPos;
+uniform float xyPerspectiveBias0;
+uniform float xyPerspectiveBias1;
+uniform vec3 shadow_tint;
+uniform vec3 dayLight;
+
+VARYING_ float adj_shadow_strength;
+VARYING_ float cosLight;
+VARYING_ float f_normal_length;
+VARYING_ vec3 shadow_position;
+VARYING_ float perspective_factor;
+
+#if __VERSION__ >= 130
+#define mtsmoothstep smoothstep
+#else
+float mtsmoothstep(in float edge0, in float edge1, in float x)
+{
+	float t = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+	return t * t * (3.0 - 2.0 * t);
+}
+#endif
+
+vec3 getLightSpacePosition()
+{
+	return shadow_position * 0.5 + 0.5;
+}
+
+float getHardShadow(sampler2D shadowsampler, vec2 smTexCoord, float realDistance)
+{
+	float texDepth = texture2D(shadowsampler, smTexCoord.xy).r;
+	return step(0.0, realDistance - texDepth);
+}
+
+#define PCFBOUND 1.0
+#define PCFSAMPLES 9
+
+float getShadow(sampler2D shadowsampler, vec2 smTexCoord, float realDistance)
+{
+	return getHardShadow(shadowsampler, smTexCoord.xy, realDistance);
+}
 #endif
 
 void main(void)
@@ -17,5 +69,41 @@ void main(void)
 	}
 
 	vec4 finalColor = texColor * varColor;
+
+#ifdef ENABLE_DYNAMIC_SHADOWS
+	if (f_shadow_strength > 0.0) {
+		float shadow_int = 0.0;
+		vec3 posLightSpace = getLightSpacePosition();
+
+		float distance_rate = (1.0 - pow(clamp(2.0 * length(posLightSpace.xy - 0.5), 0.0, 1.0), 10.0));
+		if (max(abs(posLightSpace.x - 0.5), abs(posLightSpace.y - 0.5)) > 0.5)
+			distance_rate = 0.0;
+		float f_adj_shadow_strength = max(adj_shadow_strength - mtsmoothstep(0.9, 1.1, posLightSpace.z), 0.0);
+
+		if (distance_rate > 1e-7) {
+			if (cosLight > 0.0 || f_normal_length < 1e-3)
+				shadow_int = getShadow(ShadowMapSampler, posLightSpace.xy, posLightSpace.z);
+			else
+				shadow_int = 1.0;
+			shadow_int *= distance_rate;
+			shadow_int = clamp(shadow_int, 0.0, 1.0);
+		}
+
+		float adjusted_night_ratio = pow(max(0.0, nightRatio), 0.6);
+
+		const float self_shadow_cutoff_cosine = 0.035;
+		if (f_normal_length != 0.0 && cosLight < self_shadow_cutoff_cosine) {
+			shadow_int = max(shadow_int, 1.0 - clamp(cosLight, 0.0, self_shadow_cutoff_cosine) / self_shadow_cutoff_cosine);
+		}
+
+		shadow_int *= f_adj_shadow_strength;
+
+		finalColor.rgb =
+			adjusted_night_ratio * finalColor.rgb +
+			(1.0 - adjusted_night_ratio) * (
+				finalColor.rgb * (1.0 - shadow_int));
+	}
+#endif
+
 	gl_FragData[0] = finalColor;
 }
--- nodes_shader/opengl_vertex.glsl
+++ nodes_shader/opengl_vertex.glsl
@@ -1,6 +1,10 @@
+uniform mat4 mWorld;
 uniform vec3 dayLight;
 
 VARYING_ vec4 varColor;
+VARYING_ vec3 vNormal;
+VARYING_ vec3 worldPosition;
+VARYING_ float nightRatio;
 
 #if USE_ARRAY_TEXTURE
 VARYING_ vec3 varTexCoord;
@@ -10,12 +14,69 @@ VARYING_ vec2 varTexCoord;
 
 const vec3 artificialLight = vec3(1.04, 1.04, 1.04);
 
+#ifdef ENABLE_DYNAMIC_SHADOWS
+uniform vec3 v_LightDirection;
+uniform float f_textureresolution;
+uniform mat4 m_ShadowViewProj;
+uniform float f_shadowfar;
+uniform float f_shadow_strength;
+uniform float f_timeofday;
+uniform vec4 CameraPos;
+uniform float xyPerspectiveBias0;
+uniform float xyPerspectiveBias1;
+uniform float zPerspectiveBias;
+
+VARYING_ float cosLight;
+VARYING_ float adj_shadow_strength;
+VARYING_ float f_normal_length;
+VARYING_ vec3 shadow_position;
+VARYING_ float perspective_factor;
+
+vec4 getRelativePosition(in vec4 position)
+{
+	vec2 l = position.xy - CameraPos.xy;
+	vec2 s = l / abs(l);
+	s = (1.0 - s * CameraPos.xy);
+	l /= s;
+	return vec4(l, s);
+}
+
+float getPerspectiveFactor(in vec4 relativePosition)
+{
+	float pDistance = length(relativePosition.xy);
+	return pDistance * xyPerspectiveBias0 + xyPerspectiveBias1;
+}
+
+vec4 applyPerspectiveDistortion(in vec4 position)
+{
+	vec4 l = getRelativePosition(position);
+	float pFactor = getPerspectiveFactor(l);
+	l.xy /= pFactor;
+	position.xy = l.xy * l.zw + CameraPos.xy;
+	position.z *= zPerspectiveBias;
+	return position;
+}
+
+#if __VERSION__ >= 130
+#define mtsmoothstep smoothstep
+#else
+float mtsmoothstep(in float edge0, in float edge1, in float x)
+{
+	float t = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+	return t * t * (3.0 - 2.0 * t);
+}
+#endif
+#endif
+
 void main(void)
 {
 	gl_Position = mWorldViewProj * inVertexPosition;
 
+	worldPosition = (mWorld * inVertexPosition).xyz;
+	vNormal = inVertexNormal;
+
 	vec4 color = inVertexColor;
-	float nightRatio = 1.0 - color.a;
+	nightRatio = 1.0 - color.a;
 	color.rgb = color.rgb * (color.a * dayLight.rgb + nightRatio * artificialLight.rgb) * 2.0;
 	color.a = 1.0;
 	varColor = clamp(color, 0.0, 1.0);
@@ -25,4 +86,46 @@ void main(void)
 	#else
 	varTexCoord = inTexCoord0.st;
 	#endif
+
+#ifdef ENABLE_DYNAMIC_SHADOWS
+	if (f_shadow_strength > 0.0) {
+		vec4 shadow_pos = inVertexPosition;
+		vec3 nNormal;
+		f_normal_length = length(vNormal);
+
+		float normalOffsetScale, z_bias;
+		float pFactor = getPerspectiveFactor(getRelativePosition(m_ShadowViewProj * mWorld * shadow_pos));
+		if (f_normal_length > 0.0) {
+			nNormal = normalize(vNormal);
+			cosLight = max(1e-5, dot(nNormal, -v_LightDirection));
+			float sinLight = pow(1.0 - pow(cosLight, 2.0), 0.5);
+			normalOffsetScale = 2.0 * pFactor * pFactor * sinLight * min(f_shadowfar, 500.0) /
+					xyPerspectiveBias1 / f_textureresolution;
+			z_bias = 1.0 * sinLight / cosLight;
+		} else {
+			nNormal = vec3(0.0);
+			cosLight = clamp(dot(v_LightDirection, normalize(vec3(v_LightDirection.x, 0.0, v_LightDirection.z))), 1e-2, 1.0);
+			float sinLight = pow(1.0 - pow(cosLight, 2.0), 0.5);
+			normalOffsetScale = 0.0;
+			z_bias = 3.6e3 * sinLight / cosLight;
+		}
+		z_bias *= pFactor * pFactor / f_textureresolution / f_shadowfar;
+
+		shadow_position = applyPerspectiveDistortion(m_ShadowViewProj * mWorld * (shadow_pos + vec4(normalOffsetScale * nNormal, 0.0))).xyz;
+		shadow_position.z -= z_bias;
+		perspective_factor = pFactor;
+
+		if (f_timeofday < 0.2) {
+			adj_shadow_strength = f_shadow_strength * 0.5 *
+				(1.0 - mtsmoothstep(0.18, 0.2, f_timeofday));
+		} else if (f_timeofday >= 0.8) {
+			adj_shadow_strength = f_shadow_strength * 0.5 *
+				mtsmoothstep(0.8, 0.83, f_timeofday);
+		} else {
+			adj_shadow_strength = f_shadow_strength *
+				mtsmoothstep(0.20, 0.25, f_timeofday) *
+				(1.0 - mtsmoothstep(0.7, 0.8, f_timeofday));
+		}
+	}
+#endif
 }
